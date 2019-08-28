61c6b7c5452cf5bd4e481c39ca17eaa211bbe3fd
Christian Fagan <ChristianFagan11@gmail.com>
2019-08-28 16:31:40 -0700

    Add files via upload

--- Random_City_Network.py
+++ Random_City_Network.py
@@ -9,114 +9,138 @@ city_data = pd.read_csv('uscitiesv1.5.csv')
 
 cities = city_data['city'].unique().tolist()
 
-
-### added a 'less random' graph generator for easier testing
 def city_generator(intersections):
+    """
+    This function uses the NetworkX package to generate graphs that act as synthetic data for city streets.
+
+    Attributes:
+            intersections (list): A set of graph nodes that represent the intersection of city streets.
+
+    """
     city = nx.DiGraph()
 
     num_intersections = len(intersections)
 
-
     for x in intersections:
-        city.add_node(x, stop_type = np.random.choice(['None','Stop Sign', 'Stop Light']))
+        city.add_node(x, stop_type =np.random.choice(['None','Stop Sign', 'Stop Light']))
 
-        possible_streets = [(x,y)for (x,y) in itertools.product(intersections,intersections) if x != y]
+        possible_streets = [(x, y) for (x, y) in itertools.product(intersections, intersections) if x != y]
 
-    num_streets = random.randint(num_intersections - 1, 2*(num_intersections-1))
+    num_streets = random.randint(num_intersections - 1, 2 * (num_intersections - 1))
 
-    streets = random.sample(possible_streets,num_streets)
+    streets = random.sample(possible_streets, num_streets)
 
-    for (x,y) in streets:
+    for (x, y) in streets:
 
-        city.add_edge(x, y, grade = st.beta.rvs(1.5,15, size = 1)[0],
-                    lane_type = np.random.choice(['None','Unprotected','Protected']), 
-                    road_condition = st.uniform.rvs(loc = 0, scale = 1, size = 1)[0],
-                    traffic_conditions = st.beta.rvs(4,8, size = 1)[0],
-                    length = st.uniform.rvs(loc = 0, scale = 10, size = 1)[0], weight = 0)
-
-    
+        city.add_edge(x, y, 
+                    grade= st.beta.rvs(1.5,15, size = 1)[0],
+                    lane_type= np.random.choice(['None','Unprotected','Protected']), 
+                    road_condition= st.uniform.rvs(loc = 0, scale = 1, size = 1)[0],
+                    traffic_conditions= st.beta.rvs(4,8, size = 1)[0],
+                    length= st.uniform.rvs(loc = 0, scale = 10, size = 1)[0], weight = 0
+                    )
     return city
 
 def random_city_generator(num_intersections):
-    
+    """
+    This function uses the NetworkX package to generate random graphs that act as synthetic data for city streets.
+
+    Attributes:
+            num_intersections (int): A set of graph nodes that represent the intersection of city streets.
+            The intersections are represented by the name of a US city as imported from the uscities1.5.csv
+            file.
+
+    """
+
     city = nx.DiGraph()
 
-    # These are the nodes of the graph- the intersection (literally) of edges
     intersections = random.sample(cities, num_intersections)
 
-
-    # The randomness here doesn't need to be uniform, and that ultimately doesn't reflect reality
-    # in all likelihood- not sure where *realistic* data would come from, and I couldn't find a
-    # straightforward way to generate random probability vectors p = <p_1,p_2,p_3> 
     for x in intersections:
         city.add_node(x, stop_type = np.random.choice(['None', 'Stop Sign', 'Stop Light']))
 
+    possible_streets = [(x, y) for (x, y) in itertools.product(intersections, intersections) if x != y]
 
-    # The graph doesn't need to be entirely connected, but these are all possible edges
-    possible_streets = [(x,y )for (x,y) in itertools.product(intersections,intersections) if x != y]
+    num_streets = random.randint(num_intersections - 1, 2 * (num_intersections - 1))
 
-    # Determines how many edges are in the graph
-    num_streets = random.randint(num_intersections - 1, 2*(num_intersections-1))
+    streets = random.sample(possible_streets, num_streets)
 
-    # The randomly selected edges of the graph
-    streets = random.sample(possible_streets,num_streets)
+    for (x, y) in streets:
 
-    # Adds attributes to each street- the grade (distribution is heavily weighted to not having unrealistically
-    # steep streets), discrete possibilities for bike lane types, uniform variables for length and road conditions
-    # (guess these could be N(0,1) if necessary but make a lot of sense in this package, a beta distribution
-    # that allows for the possibility of heavy traffic but weighs moderate traffic nicely, and initializing
-    # the weight to be 0 at first so that the user choice can determine weight
-    for (x,y) in streets:
-
-        city.add_edge(x, y, grade = st.beta.rvs(1.5,15, size = 1),
+        city.add_edge(x, y, 
+                    grade = st.beta.rvs(1.5,15, size = 1),
                     lane_type = np.random.choice(['None','Unprotected','Protected']), 
                     road_condition = st.uniform.rvs(loc = 0, scale = 1, size = 1),
                     traffic_conditions = st.beta.rvs(4,8, size = 1),
-                    length = st.uniform.rvs(loc = 0, scale = 10, size = 1), weight = 0)
-
-    
+                    length = st.uniform.rvs(loc = 0, scale = 10, size = 1), weight = 0
+                    )
     return city
     
-def simple_route_weigher(city,attribute):
-    # This can turn numerical attributes directly into weights. Looking for shortest paths
-    # matches the modeling features from the city generator as more traffic, steeper, etc = bad.
-    for (x,y) in city.edges():
-        g[x][y]['weight']= g[x][y][attribute]
+def simple_route_weigher(city, street_attribute):
+    """
+    This function creates weights for the edges of a given city network according to a 
+    chosen street attribute.
+        
+    Attributes:
+        city (nx.DiGraph): A city network compiled using the city_network() or random_city_network() functions
+        street_attribute (any): The grade, lane type, road condition, etc. metric chosen to weigh the edges.
+    """
 
-def simple_uniform_route_weigher(city,attributes):
-    # Evenly weighs each attribute entered
-    for(x,y) in city.edges():
-        for attribute in attributes:
-            g[x][y]['weight'] += g[x][y][attribute]/len(attributes)
+    for (x, y) in city.edges():
+        g[x][y]['weight']= g[x][y][street_attribute]
 
-def route_weigher(city,attributes,weights):
-    ## the route weights can be normalized to make sure the edge weights are all less than 1 
-    ## if it matters for the neural network 
+def simple_uniform_route_weigher(city, street_attributes):
+    """
+    This function creates weights for the edges of a given city network by weighing the 
+    chosen street attributes evenly.
+        
+    Attributes:
+        city (nx.DiGraph): A city network compiled using the city_network() or random_city_network() functions
+        street_attributes (list): The set of grade, lane type, road condition, etc. metrics chosen to weigh the edges.
+    """
 
-    for (x,y) in city.edges():
-        for i in range(len(attributes):
-            g[x][y]['weight'] += weights[i]*attributes[i]
+    for(x, y) in city.edges():
+        for attribute in street_attributes:
+            g[x][y]['weight'] += g[x][y][attribute]/len(street_attributes)
+
+def route_weigher(city, street_attributes, street_attribute_weights):
+    """
+    This function creates weights for a given city network by weighing the chosen street attributes evenly.
+        
+    Attributes:
+        city (nx.DiGraph): A city network compiled using the city_network() or random_city_network() functions
+        street_attributes (list): The set of grade, lane type, road condition, etc. metrics chosen to weigh the edges.
+        street_attribute_weights (np.array): A vector of weights to apply to each of the individual street attributes
+        to form a linear combination of the given street attributes to form the weight of each edge.
+    """
+
+    for (x, y) in city.edges():
+        for i in range(len(street_attributes)):
+            g[x][y]['weight'] += street_attribute_weights[i]*street_attributes[i]
 
 
-#### This function also does constrained search according to one of the parameters. It would also
-#### be possible to change the single attribute to a linear combination of attributes as above. 
-#### This seems a little more stilted since A* does essentially the same thing just by treating
-#### the constraint of interest as weights- it's a cardinal and not an ordinal problem from the 
-#### perspective of choosing graphs, but it's certainly an ordinal problem once we want to start
-#### doing inference. 
-def minimizing_search(graph,node_1,node_2,attribute):
+def minimizing_search(graph, node_1, node_2, street_attribute):
+    """
+    This function finds the shortest distance from node_1 to node_2. 
+
+    Attributes:
+        graph (nx.DiGraph): The network being searched.
+        node_1 (nx.DiGraph.node): The first node in the desired route.
+        node_2 (nx.DiGraph.node): The final node in the desired route.
+        street_attribute (any): The criteria to be used as weight for edges.
+    """
     path = []
     dead_ends = []
-    if nx.has_path(graph,node_1,node_2) == True:
+    if nx.has_path(graph, node_1, node_2) == True:
         starting_point = node_1
         path.append(starting_point)
         while starting_point != node_2:
-            possible_stops = [x for x in nx.neighbors(graph,starting_point) if x not in path]
+            possible_stops = [x for x in nx.neighbors(graph, starting_point) if x not in path]
             if possible_stops == []:
                 dead_ends.append(starting_point)
                 path = path[:len(path-1)]
                 starting_point = path[len(path)-1]
-                possible_stops = [x for x in nx.neighbors(graph,starting_point) if x not in path
+                possible_stops = [x for x in nx.neighbors(graph, starting_point) if x not in path
                                             and x not in dead_ends]
 
             slopes = [graph[starting_point][x][attribute] for x in possible_stops]
@@ -126,4 +150,5 @@ def minimizing_search(graph,node_1,node_2,attribute):
        
     else:
         print('Can\'t take that route, sorry!')
-    return path    
+
+    return path
